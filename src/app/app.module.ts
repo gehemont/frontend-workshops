2bbe73eebcb40d3e1d19472002bca2a80ade8447
lukasz.solana <lukasz.solana@softwareone.com>
2019-11-28 14:00:00 +0100

    store freez

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { rootReducer } from './store';
+import { MetaReducer, StoreModule } from '@ngrx/store';
+import { ApplicationState, rootReducer } from './store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { ProductEditComponent } from './components/product-edit/product-edit.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -11,6 +11,10 @@ import { Demo1Component } from './demo/demo1/demo1.component';
 import { Demo2Component } from './demo/demo2/demo2.component';
 import { Demo3Component } from './demo/demo3/demo3.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
+import { storeFreeze } from 'ngrx-store-freeze';
+import { environment } from '../environments/environment'; // Angular CLI environment
+
+export const metaReducers: MetaReducer<ApplicationState>[] = !environment.production ? [storeFreeze] : [];
 
 @NgModule({
   declarations: [
@@ -24,7 +28,7 @@ import { ProductsListComponent } from './components/products-list/products-list.
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot(rootReducer),
+    StoreModule.forRoot(rootReducer, { metaReducers }),
     StoreDevtoolsModule.instrument(),
     ReactiveFormsModule
   ],
